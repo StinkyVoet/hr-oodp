b5d73710bf945569f9f708316c3938df4cf91368
StinkyVoet <stinkyvoet@gmail.com>
2023-03-06 11:02:54 +0100

    .

---
+++ a1/w04/1/c06/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+
+class Program
+{
+    public static void Main()
+    {
+        try
+        {
+            StreamReader reader = new("People.json");
+            var file2Json = reader.ReadToEnd();
+            var people = JsonConvert.DeserializeObject<List<Person>>(file2Json)!;
+            reader.Close();
+            reader.Dispose();
+
+            foreach (var car in people.SelectMany(person => person.OwnedCars))
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    car.Drive();
+                }
+            }
+            
+            StreamWriter writer = new("People.json");
+            var list2Json = JsonConvert.SerializeObject(people);
+            writer.Write(list2Json);
+            writer.Close();
+            writer.Dispose();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
